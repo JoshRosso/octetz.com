67e8c664be146a9562d752e8e734212f746a3717
joshrosso <joshrosso@gmail.com>
2017-11-16 08:48:19 -0800

    restructure

---
+++ update-site.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+LOG_PREFIX="============="
+SERVER_LOCATION="/usr/share/nginx/html"
+
+echo
+echo "$LOG_PREFIX Updating sites"
+echo
+
+# update joshrosso.com
+echo
+echo "$LOG_PREFIX Updating joshrosso.com"
+echo
+cp -rv index.html $SERVER_LOCATION
+
+# update octetz.com
+echo
+echo "$LOG_PREFIX Updating octetz.com"
+echo
+cp -rv ./octetz ${SERVER_LOCATION}/octetz
+
+# update vanhabits.com
+echo
+echo "$LOG_PREFIX Updating vanhabits.com"
+echo
+cp -rv ./vanhabits ${SERVER_LOCATION}/vanhabits
